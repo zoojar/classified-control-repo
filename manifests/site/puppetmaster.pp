af05d5581d0a07885f0408a9e1af3b2ccc007e6c
zoojar <davidnewton82@gmail.com>
2016-02-18 02:25:20 +0000

    added puppetmaster class for eyaml config

--- manifests/site/puppetmaster.pp
+++ manifests/site/puppetmaster.pp
@@ -1,3 +1,13 @@
 class puppetmaster {
 
+  class { '::hiera':
+    datadir   => '/etc/puppetlabs/puppet/hieradata',
+    hierarchy => [
+      '%{environment}/%{calling_class}',
+      '%{environment}',
+      'common',
+    ],
+  }
+
+
 }
